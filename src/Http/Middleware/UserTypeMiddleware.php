5babc030bda0bd737181df0bb47516fa56236bc9
Hani Majebry <hani.majebry@gmail.com>
2019-12-18 17:29:25 +0200

    Fix user-type model namespace

--- src/Http/Middleware/UserTypeMiddleware.php
+++ src/Http/Middleware/UserTypeMiddleware.php
@@ -16,11 +16,14 @@ class UserTypeMiddleware
      */
     public function handle($request, Closure $next, $userType)
     {
-        $model = 'App\\' . Str::studly($userType);
+        $model = [
+            'Majebry\\LaravelAuthWrapper\\' . Str::studly($userType),
+            'App\\' . Str::studly($userType)
+        ];
 
-        $user = auth()->user();
-        
-        if (!$user || ($user && $user->userable_type != $model)) {
+        $user = auth('api')->user();
+
+        if (!$user || ($user && !in_array($user->userable_type, $model))) {
             abort(403);
         }
 
